38fdd04ff36e4e6a38ae39b15f743abb50e10e37
Lucas Lacerda <lucaslacerda.jlbl@gmail.com>
2021-06-30 14:43:07 -0300

    Project create

---
+++ SaleWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaleWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
